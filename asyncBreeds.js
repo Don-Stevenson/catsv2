fae229d0159c11e6b9630a76ef4c73247dc6eb30
Don-Stevenson <dsteven1@gmail.com>
2019-12-04 01:01:46 +0000

    working versions of the files. All tests working

---
+++ asyncBreeds.js
@@ -0,0 +1,20 @@
+// asyncBreeds.js
+const fs = require('fs');
+
+const breedDetailsFromFile = function(breed, callback) {
+  console.log('breedDetailsFromFile: Calling readFile...');
+  
+  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+        console.log('Callback: I have the data!')
+    if (!error) callback(data); 
+    if (error) callback(undefined);
+ })
+}
+
+
+
+breedDetailsFromFile('Bombay', (x) => {
+console.log('Return Value: ', x) 
+});
+
+module.exports = breedDetailsFromFile;
